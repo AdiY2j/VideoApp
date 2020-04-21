d1ee7509dae626993a507d28e91ca0dbff4e3eaa
Adityakumar Yadav <adityakumar.y@somaiya.edu>
2020-04-21 10:25:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/videoapp/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.videoapp;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+
+import vimeoextractor.OnVimeoExtractionListener;
+import vimeoextractor.VimeoExtractor;
+import vimeoextractor.VimeoVideo;
+
+public class MainActivity extends AppCompatActivity implements Player.EventListener {
+
+
+    private SimpleExoPlayer player;
+    private ProgressBar progressBar;
+    private PlayerView playerView;
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
+    private String youtubeUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        playerView = findViewById(R.id.video_view);
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    private void initializePlayer(String downloadUrl) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Uri uri = Uri.parse(downloadUrl);
+                MediaSource mediaSource = buildMediaSource(uri);
+                player.setPlayWhenReady(playWhenReady);
+                player.seekTo(currentWindow, playbackPosition);
+                player.prepare(mediaSource, false, false);
+                playerView.setPlayer(player);
+            }
+        });
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, "test");
+        return new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(player == null) {
+            player = ExoPlayerFactory.newSimpleInstance(this);
+        }
+        player.addListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(TextUtils.isEmpty(youtubeUrl)) {
+            getVimeoUrl();
+        }else{
+            initializePlayer(youtubeUrl);
+        }
+
+    }
+
+    private void getVimeoUrl() {
+        progressBar.setVisibility(View.VISIBLE);
+        VimeoExtractor.getInstance().fetchVideoWithURL(getString(R.string.media_url), null, new OnVimeoExtractionListener() {
+            @Override
+            public void onSuccess(VimeoVideo video) {
+                youtubeUrl = video.getStreams().get("360p");
+                initializePlayer(youtubeUrl);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                //Error handling here
+            }
+        });
+    }
+
+    /*@SuppressLint("StaticFieldLeak")
+    private void extractYoutubeUrl() {
+        progressBar.setVisibility(View.VISIBLE);
+        new YouTubeExtractor(this) {
+            @Override
+            public void onExtractionComplete(SparseArray<YtFile> ytFiles, VideoMeta vMeta) {
+                if (ytFiles != null) {
+                    int itag = 22;
+                    youtubeUrl = ytFiles.get(itag).getUrl();
+                    initializePlayer(youtubeUrl);
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        }.extract(getString(R.string.media_url), true, true);
+    }*/
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(player != null) {
+            playWhenReady = player.getPlayWhenReady();
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if(playbackState == SimpleExoPlayer.STATE_BUFFERING || playbackState == SimpleExoPlayer.STATE_IDLE){
+            progressBar.setVisibility(View.VISIBLE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+}
